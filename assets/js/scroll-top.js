e93c2e6ee71ea62029fcc5d0b97f38c20542cb98
MrBabon <christophedanna06@gmail.com>
2024-05-16 17:32:54 +0200

    envoi en prod

--- assets/js/scroll-top.js
+++ assets/js/scroll-top.js
@@ -1,33 +1,40 @@
 export function setupScrollTop() {
-    var scrollButton = document.getElementById('scrollButton');
-    var scrollIcon = document.getElementById('scrollIcon');
+    window.onload = function() {
+        var scrollButton = document.getElementById('scrollButton');
+        var scrollIcon = document.getElementById('scrollIcon');
 
-    function updateScroll() {
-        if (window.scrollY <= 0) {
-            scrollIcon.classList.remove('opacity-1');
-            scrollIcon.classList.add('opacity-0');
-        } else {
-            scrollIcon.classList.remove('opacity-0');
-            scrollIcon.classList.add('opacity-1');
+        if (!scrollButton || !scrollIcon) {
+            console.error('scrollButton or scrollIcon not found in the document.');
+            return;
         }
-    }
 
-    window.addEventListener('scroll', updateScroll);
+        function updateScroll() {
+            if (window.scrollY <= 0) {
+                scrollIcon.classList.remove('opacity-1');
+                scrollIcon.classList.add('opacity-0');
+            } else {
+                scrollIcon.classList.remove('opacity-0');
+                scrollIcon.classList.add('opacity-1');
+            }
+        }
 
-    scrollButton.addEventListener('click', function (event) {
-        event.preventDefault();
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
+        window.addEventListener('scroll', updateScroll);
+
+        scrollButton.addEventListener('click', function (event) {
+            event.preventDefault();
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
         });
-    });
 
-    // Appeler la fonction updateScroll une fois au chargement de la page
-    updateScroll();
+        // Appeler la fonction updateScroll une fois au chargement de la page
+        updateScroll();
 
-    // Assurez-vous de retirer les écouteurs d'événements lorsque la page se ferme
-    window.addEventListener('beforeunload', function () {
-        window.removeEventListener('scroll', updateScroll);
-        scrollButton.removeEventListener('click', function () {});
-    });
+        // Assurez-vous de retirer les écouteurs d'événements lorsque la page se ferme
+        window.addEventListener('beforeunload', function () {
+            window.removeEventListener('scroll', updateScroll);
+            scrollButton.removeEventListener('click', function () {});
+        });
+    };
 }
